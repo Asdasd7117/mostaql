ecb2f0dafffcfc6b949efed6665dfbade1849643
Asdasd7117 <alywm100@gmail.com>
2026-05-01 02:50:11 +0300

    Refactor SupabaseClient to SupabaseClientProvider

--- app/src/main/java/com/example/mostaqlapp/data/SupabaseClient.kt
+++ app/src/main/java/com/example/mostaqlapp/data/SupabaseClient.kt
@@ -1,17 +1,16 @@
 package com.example.mostaql.data
 
 import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 
-object SupabaseClient {
+object SupabaseClientProvider {
 
-    val client = createSupabaseClient(
+    val client: SupabaseClient = createSupabaseClient(
         supabaseUrl = "https://ojposlwuwtvzypgpdemm.supabase.co",
         supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qcG9zbHd1d3R2enlwZ3BkZW1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0NDk5MzgsImV4cCI6MjA5MzAyNTkzOH0.ApdGwZvuzr5IgtSvEK_F3thY5FNsFR7qG6_mcy89eQ0"
     ) {
-
-        // ✅ تفعيل الموديولات بشكل صحيح
         install(Auth)
         install(Postgrest)
     }
