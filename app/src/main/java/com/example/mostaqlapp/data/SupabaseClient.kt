11655f9f53305cd2d7a9575670cb83b1c6fe9ace
Asdasd7117 <alywm100@gmail.com>
2026-05-01 03:17:38 +0300

    Update SupabaseClient.kt

--- app/src/main/java/com/example/mostaqlapp/data/SupabaseClient.kt
+++ app/src/main/java/com/example/mostaqlapp/data/SupabaseClient.kt
@@ -1,7 +1,8 @@
 package com.example.mostaql.data
 
-import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.SupabaseClient
+
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 
@@ -11,6 +12,8 @@ object SupabaseClient {
         supabaseUrl = "https://ojposlwuwtvzypgpdemm.supabase.co",
         supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qcG9zbHd1d3R2enlwZ3BkZW1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0NDk5MzgsImV4cCI6MjA5MzAyNTkzOH0.ApdGwZvuzr5IgtSvEK_F3thY5FNsFR7qG6_mcy89eQ0"
     ) {
+
+        // 🔥 لازم تضيفهم
         install(Auth)
         install(Postgrest)
     }
