39e937965d78884819bc9c8f325c0785d482e204
Asdasd7117 <alywm100@gmail.com>
2026-05-01 03:07:11 +0300

    Reorder import statements in SupabaseClient.kt

--- app/src/main/java/com/example/mostaqlapp/data/SupabaseClient.kt
+++ app/src/main/java/com/example/mostaqlapp/data/SupabaseClient.kt
@@ -1,7 +1,7 @@
 package com.example.mostaql.data
 
-import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 
